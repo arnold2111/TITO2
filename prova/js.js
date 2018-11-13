1eccd5242850ebded6fb84be0c32aa08fe501c58
arnold2111 <43273915+arnold2111@users.noreply.github.com>
2018-11-13 16:52:32 +0100

    Add files via upload

---
+++ prova/js.js
@@ -0,0 +1,42 @@
+function hola(){
+
+    alert("this is nightcore");
+
+}
+
+function suma(numero1,numero2) {
+
+    var numero3= numero1+numero2;
+    alert(numero3);
+}
+
+function array1(){
+
+    var llistat = new Array();
+    var llistat2 = new Array(5);
+    var llistat3 = new Array("vermell","Granate","Blabooos");
+    var llistat4 = ["Blanc","Negre","Gris"];
+    var llistat5 =  [];
+
+    alert(llistat);
+    alert(llistat2);
+    alert(llistat3);
+    alert(llistat4);
+    alert(llistat5);
+
+}
+
+function array2(){
+
+    var llistat = new Array();
+    var llistat2 = new Array(5);
+    var llistat3 = new Array("vermell","Granate","Blabooos");
+    var llistat4 = ["Blanc","Negre","Gris"];
+    var llistat5 =  [];
+
+    for(var x = 0; x<llistat3;x++){
+
+        alert(llistat3.toString)
+
+    }
+}
